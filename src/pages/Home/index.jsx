8d38027024b69509d47e68fde70d00869324f2dd
guilhermeabell <gpeuvis@gmail.com>
2023-09-16 22:11:47 -0300

    feat(main): created Loader component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,13 @@
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
+import Loader from '../../components/Loader';
 import * as S from './styles';
 
 export default function Home() {
   return (
     <S.Container>
+      <Loader />
       <S.InputSearchContainer>
       <S.Input placeholder="Pesquise pelo nome..." />
       </S.InputSearchContainer>
